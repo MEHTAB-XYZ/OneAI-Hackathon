7ce2490080d5314f97a8791c18914e84239df1e6
MEHTAB <mehtabarifahad@gmail.com>
2025-07-04 23:03:51 +0530

    Clean commit with .env and no secrets

---
+++ test.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from huggingface_hub import HfApi
+
+load_dotenv()  # loads from .env
+
+token = os.getenv("HF_TOKEN")
+api = HfApi(token=token)
+user = api.whoami()
+
+print(f"✅ Token is valid! Logged in as: {user['name']}")
